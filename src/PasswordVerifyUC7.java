d4adb11347b1cea8cc8ff53fe349fc4e98c59766
KunigiMohan <kmohanr5@gmail.com>
2021-11-14 15:51:02 +0530

    Added : Added condition for password contains atleast one number.

---
+++ src/PasswordVerifyUC7.java
@@ -0,0 +1,26 @@
+
+
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
+public class PasswordVerifyUC7 {
+    /*
+     * passwordVerify() method to verify password entered by user is same according to regex
+     * */
+    public boolean passwordVerify(String password){
+        if(password==null&&password.isEmpty())      //Checking input Password is empty or not
+        {
+            return true;
+        }
+        String regex="^(?=.*[a-z])(?=.*[A-Z])(?=.*d)[a-zA-Zd]{8,}$";    //Regex code for verifying Password
+        Pattern pattern= Pattern.compile(regex);
+        if (pattern.matcher(password).matches())        //Checking entered password matches with regex code or not.
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
